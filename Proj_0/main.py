bc2e21b7f2b627b0b0a2e11d76b74e8d9baaf029
gchava20 <136290145+gchava20@users.noreply.github.com>
2025-01-14 18:43:30 -0500

    Update main.py

--- Proj_0/main.py
+++ Proj_0/main.py
@@ -1,16 +1,2 @@
-# This is a sample Python script.
+print('Answer these questions about your partner: Name and Major? What do they do when they are not here? job, hobbies, other? Fun fact about them? What is the coolest thing they have seen technology do?')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
