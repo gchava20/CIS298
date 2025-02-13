bf31595db910157ff344b494ae75dd5c6380fa03
gchava20 <136290145+gchava20@users.noreply.github.com>
2025-02-13 05:57:09 -0500

    ask initial questions and take inputs

--- RetirementCalculator.py
+++ RetirementCalculator.py
@@ -9,5 +9,17 @@ def readCSV(file_path):
             rates.append((int(year), float(stock.strip('%')) / 100, float(bond.strip('%')) / 100))
     return (rates)
 
-rates = readCSV()
-print (rates)
+def main():
+    rates = readCSV('BondsAndStocksAnnualReturn.csv')
+
+    #ask initial questions
+    age = int(input("How old are you? "))
+    retirement_age = int(input("At what age do you want to retire? "))
+
+    # take inputs
+    mattress = float(input("Please enter the starting balance for money under your mattress: "))
+    bank_savings = float(input("Please enter the starting balance for money in bank savings: "))
+    bonds = float(input("Please enter the starting balance for money in bonds: "))
+    stocks = float(input("Please enter the starting balance for money in stocks: "))
+
+    
