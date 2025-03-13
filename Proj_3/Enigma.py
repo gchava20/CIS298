7fe9cb35d106714624f9d2dd43eee27f3c168376
gchava20 <136290145+gchava20@users.noreply.github.com>
2025-03-13 07:22:39 -0400

    defined the skeleton class of enigma

---
+++ Proj_3/Enigma.py
@@ -0,0 +1,7 @@
+class EnigmaMachine:
+    def __init__(self, rotor_numbers, rotor_positions,plugboard_settings):
+        self.rotors = [] #holds rotor
+        self.plugboard = None
+        self. reflector = None
+    
+    
