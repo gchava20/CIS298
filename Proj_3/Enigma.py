8139cc38d4a9e6c69860c0b7badab269763eb1ef
gchava20 <136290145+gchava20@users.noreply.github.com>
2025-03-13 08:26:44 -0400

    created reflector class

--- Proj_3/Enigma.py
+++ Proj_3/Enigma.py
@@ -68,3 +68,16 @@ class Plugboard:
         # swap the chars
         return self.wiring[char]
     
+class Reflector:
+    def __init__(self, type='B'):
+        #reflector wirings
+        self.wirings = {
+            'A': "EJMZALYXVBWFCRQUONTSPIKHGD", #A wiring
+            'B': "YRUHQSLDPXNGOKMIEBFZCWVJAT", #B wiring
+            'C': "FVPJIAOYEDRZXWGCTKUQSBNMHL"  #C wiring
+        }
+        self.mapping = self.wirings[type]
+
+    def reflect(self, char):
+        index = ord(char) - ord('A')
+        return self.mapping[index]
