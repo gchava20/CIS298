4f606ae7fe805160a1e1135cca97498311523f7a
gchava20 <136290145+gchava20@users.noreply.github.com>
2025-03-13 07:40:38 -0400

    created rotor class

--- Proj_3/Enigma.py
+++ Proj_3/Enigma.py
@@ -1,7 +1,28 @@
 class EnigmaMachine:
-    def __init__(self, rotor_numbers, rotor_positions,plugboard_settings):
+    def __init__(self, rotor_num, rotor_pos,plug_settings):
         self.rotors = [] #holds rotor
         self.plugboard = None
         self. reflector = None
     
-    
+    def encode_message(self, message):
+
+        pass
+
+
+class Rotor:
+    def __init__(self, rotor_num, start_pos):
+
+        #historical rotor mappings
+        self.rotor_mappings = {
+            1: "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+            2: "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+            3: "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+            4: "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+            5: "VZBRGITYUPSDNHLXAWMJQOFECK"
+        }
+
+        self.notch_pos = {
+        # turns the rotor values
+        
+        }
+
