fcd8c0729e1a3ae311b3dacaf943c1efe21600fd
gchava20 <136290145+gchava20@users.noreply.github.com>
2025-03-13 08:12:26 -0400

    def plugboard + def encode
    Initialized the wiring based on the settings. Applied the settings to swap character pairs. Swapped the character if it's involved in any plugboard pair.

--- Proj_3/Enigma.py
+++ Proj_3/Enigma.py
@@ -55,3 +55,16 @@ class Rotor:
         self.position = (self.position + 1) % 26
         return self.position == (ord(self.notch) - ord('A'))
     
+class Plugboard:
+    def __init__(self, settings):
+        self.wiring = {chr(i): chr(i) for i in range(ord('A'), ord('Z')+1)}
+        #swap pairs of chars
+        for pair in settings.split():
+            if len(pair) == 2:
+                a, b = pair[0], pair[1]
+                self.wiring[a], self.wiring[b] = b, a
+
+    def encode(self, char):
+        # swap the chars
+        return self.wiring[char]
+    
